c86b9812c23773ce518a9ba29ec6eff2e322b149
Cheri Hung <cherihung@gmail.com>
2020-01-08 23:10:46 -0500

    env support

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import express from 'express';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
-/* 
-set up all your server config here
-*/
+
 app.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 var nodeExternals = require("webpack-node-externals")
 const path = require('path');
 
-module.exports = {
-    entry: './index.js',
-    target: 'node',
-    mode: 'development',
-    externals: nodeExternals(),
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'dist.js'
-    },
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                exclude: /(node_modules)/,
-                test: /\.js$/
-            }
-        ]
-    }
+var configs = {
+  entry: './index.js',
+  target: 'node',
+  mode: process.env.NODE_ENV,
+  externals: nodeExternals(),
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'dist.js'
+  },
+  module: {
+      rules: [
+          {
+              use: 'babel-loader',
+              exclude: /(node_modules)/,
+              test: /\.js$/
+          }
+      ]
+  }
+};
+
+if(process.env.NODE_ENV !== 'production') {
+  configs.devtool = 'source-map';
 }
+
+module.exports = configs;
