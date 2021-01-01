011bfbc93642fc628516df20ae8f1a561215bd29
Cheri Hung <cherihung@gmail.com>
2020-12-31 20:45:27 -0500

    rearrange

--- index.ts
+++
@@ -1,5 +0,0 @@
-import app from './src/app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`App listening on :${PORT}!`));
--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,4 @@
 import {LOG_FORMAT_TYPES} from './types';
 
 export const LOG_FORMAT: LOG_FORMAT_TYPES = process.env.LOG_FORMAT as LOG_FORMAT_TYPES || 'combined';
+export const PORT: string = process.env.PORT || '3000';
